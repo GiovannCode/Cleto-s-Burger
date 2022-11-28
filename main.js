8d8ff4ec95fa825bde6e2aeff2cf698a880c4d27
Giovanni <109935769+GiovannCode@users.noreply.github.com>
2022-11-27 22:08:21 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+//Scroll Reveal
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '40px',
+    duration: 2000,
+    reset: true
+});
+
+
+sr.reveal('.home-text,.home-img, .about-img .about-text, .box, .s-box, .connect-text, .btn, .contact-box, .btn2',{interval:200});
